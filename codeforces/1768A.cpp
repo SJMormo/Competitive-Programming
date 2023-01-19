dd9f0d897b19794c41cfecc2dbeb57cb39c4b973
SJMormo <sjm91097@gmail.com>
2023-01-19 11:32:32 +0600

    1768

---
+++ codeforces/1768A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define INF 1e18
+
+void fastio()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+void solve()
+{
+	ll n;
+	cin >> n;
+	cout << n - 1 << '\n';
+}
+
+int main()
+{
+	fastio();
+
+	int t = 1;
+	cin >> t;
+
+	while (t--)
+		solve();
+
+	return 0;
+}
