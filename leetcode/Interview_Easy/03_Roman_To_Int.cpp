0e7c66c0c15ad04e150d6c33203f9370805d4280
SJMormo <sjm91097@gmail.com>
2023-01-27 16:11:50 +0600

    Runtime: 44.79%, Memory: 45.58%

--- leetcode/Interview_Easy/03_Roman_To_Int.cpp
+++ leetcode/Interview_Easy/03_Roman_To_Int.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'I' && s[i + 1] == 'V')
-                ans += 4, ++i;
-            else if (s[i] == 'I' && s[i + 1] == 'X')
-                ans += 9, ++i;
-            else if (s[i] == 'X' && s[i + 1] == 'L')
-                ans += 40, ++i;
-            else if (s[i] == 'X' && s[i + 1] == 'C')
-                ans += 90, ++i;
-            else if (s[i] == 'C' && s[i + 1] == 'D')
-                ans += 400, ++i;
-            else if (s[i] == 'C' && s[i + 1] == 'M')
-                ans += 900, ++i;
-            else if (s[i] == 'M')
-                ans += 1000;
-            else if (s[i] == 'D')
-                ans += 500;
-            else if (s[i] == 'C')
-                ans += 100;
-            else if (s[i] == 'L')
-                ans += 50;
-            else if (s[i] == 'X')
-                ans += 10;
-            else if (s[i] == 'V')
-                ans += 5;
+        unordered_map <char, int> mp{
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        int ans = 0, sz = s.size();
+        for (int i = 0; i < sz; i++) {
+            if (i != sz - 1 && mp[s[i]] < mp[s[i + 1]])
+                ans -= mp[s[i]];
             else
-                ans++;
+                ans += mp[s[i]];
         }
         return ans;
     }
