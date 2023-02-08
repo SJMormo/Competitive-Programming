a0e3795d7d1ca8b00f3ab50acca2a7fb8ba9deae
SJMormo <sjm91097@gmail.com>
2023-02-09 00:14:39 +0600

    Runtime: 58%, Memory: 88%

---
+++ leetcode/Interview_Easy/06_Plus One.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(), digits.end());
+
+        for (int i = 0; i < digits.size(); i++) {
+            digits[i]++;
+
+            cout << digits[i] << endl;
+            if (i == digits.size() - 1 && digits[i] == 10) {
+                digits[i] = 0;
+                digits.push_back(1);
+                break;
+            }
+            else if (digits[i] == 10)
+                digits[i] = 0;
+            else break;
+        }
+
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
