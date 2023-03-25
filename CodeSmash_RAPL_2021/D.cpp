366f790d2e1622bba2d332a838ce0b77060d3324
SJMormo <sjm91097@gmail.com>
2023-03-25 12:37:05 +0600

    CodeSmash 2021 D

--- CodeSmash_RAPL_2021/D.cpp
+++ CodeSmash_RAPL_2021/D.cpp
@@ -13,68 +13,14 @@ void fastio()
 	cout.tie(NULL);
 }
 
-ll to_int(string s)
-{
-	reverse(s.begin(), s.end());
-	ll ans = 1, mul = 1;
-
-	for (ll i = 0; i < s.size(); i++) {
-		ll x = s[i] - 48;
-		ans += (x * mul);
-		mul *= 10;
-	}
-	return ans;
-}
-
-string to_st(ll x)
-{
-	string s = "";
-
-	while (x) {
-		ll m = x % 10;
-		char n = m + 48;
-		s += n;
-		x /= 10;
-	}
-
-	reverse(s.begin(), s.end());
-	return s;
-}
-
 void solve()
 {
-	string n;
-	ll k;
-	cin >> n >> k;
+	int a, b;
+	cin >> a >> b;
 
-	while (k--) {
-		string m = n;
-		sort(n.begin(), n.end());
-
-		ll z = 4 - m.size();
-		for (ll i = 0; i < z; i++)
-			m += '0';
-		sort(m.begin(), m.end());
-		reverse(m.begin(), m.end());
-
-		ll q = to_int(n);
-		ll p = to_int(m);
-
-		ll x = p - q;
-		if (x == 0) {
-			cout << 0 << '\n';
-			return;
-		}
-		string st = to_st(x);
-		if (n == st) {
-			cout << n << endl;
-			return;
-		}
-		n = st;
-		// cout << n << endl;
-	}
-
-	cout << n << '\n';
+	int x = 360 / b;
+	if (x & 1) cout << "NO\n";
+	else cout << "YES\n";
 }
 
 int main()
