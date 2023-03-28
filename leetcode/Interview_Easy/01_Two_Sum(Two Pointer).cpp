68e3379cc5ae76b2a85ae5ba8b444f139bf7cd38
SJMormo <sjm91097@gmail.com>
2023-03-28 23:20:01 +0600

    Sorted leetcode directory

--- leetcode/Interview_Easy/01_Two_Sum(Two Pointer).cpp
+++ leetcode/Interview_Easy/01_Two_Sum(Two Pointer).cpp
@@ -1,21 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<pair<int, int>> position;
-        for(int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
             position.push_back({nums[i], i});
         }
 
         sort(position.begin(), position.end());
         int i = 0, j = nums.size() - 1;
 
-        while(i < j) {
+        while (i < j) {
             int sum = position[i].first + position[j].first;
-            if(sum == target) {
+            if (sum == target) {
                 // cout << "[" << i << "," << j << "]";
                 return {position[i].second, position[j].second};
             }
-            else if(sum > target) {
+            else if (sum > target) {
                 --j;
             }
             else {
--- leetcode/Interview_Medium/01-rotate-image.cpp
+++ leetcode/Interview_Medium/01-rotate-image.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
