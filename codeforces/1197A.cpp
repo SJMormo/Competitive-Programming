f179763c8ebf3e57e3197ecae71f9af2c807bc6d
SJMormo <sjm91097@gmail.com>
2023-04-05 00:50:39 +0600

    1197A updated

--- codeforces/1197A.cpp
+++ codeforces/1197A.cpp
@@ -4,8 +4,6 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-const ll mod = 1000000007;
-
 #define PI acos(-1)
 #define INF 1e18
 
@@ -14,10 +12,6 @@ void fastio()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
 }
 
 void solve()
@@ -25,15 +19,14 @@ void solve()
 	int n;
 	cin >> n;
 
-	int arr[n];
-	for (int i = 0; i < n; i++) cin >> arr[i];
+	vector<int> v(n);
+	for (auto &it : v)
+		cin >> it;
 
-	sort(arr, arr + n);
-	reverse(arr, arr + n);
+	sort(v.begin(), v.end());
 
-	int res = n - 2;
-	res = min(res, arr[1] - 1);
-	cout << res << '\n';
+	int ans = min(v[n - 2] - 1, n - 2);
+	cout << ans << '\n';
 }
 
 int main()
