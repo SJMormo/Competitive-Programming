61b192e83e68f4244a1d7f7a68ef0144e80331b7
SJMormo <sjm91097@gmail.com>
2023-05-12 19:52:07 +0600

    Runtime: 134ms(73.3%), Memory: 69.4MB(65.42%)

---
+++ leetcode/Interview_Easy/217-Contains-Duplicate.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (auto it : nums) {
+            mp[it]++;
+            if (mp[it] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
