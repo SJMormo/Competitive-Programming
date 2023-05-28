10fc6f14df0d6e6e4dab0df1252b44b9c594d626
SJMormo <sjm91097@gmail.com>
2023-05-28 10:28:23 +0600

    Runtime: 127ms, Memory: 93.2MB

--- leetcode/Interview_Easy/121-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ leetcode/Interview_Easy/121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -5,12 +5,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int ans = 0, mn = prices[0];
-        for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] > prices[i - 1]) {
-                mn = min(prices[i - 1], mn);
-            }
-            ans = max(ans, prices[i] - mn);
-            cout << ans << endl;
+        for (auto it : prices) {
+            mn = min(mn, it);
+            ans = max(ans, it - mn);
         }
         return ans;
     }
